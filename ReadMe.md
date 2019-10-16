de3c9f62874e9ecb32ddf91aa7eee6e867a33f44
Tom <tom.marx@hotmail.fr>
2019-10-16 11:04:25 +0200

    Path

--- ReadMe.md
+++ ReadMe.md
@@ -6,16 +6,15 @@ The purpose of tis script is to force the project libft2019 of school 42 to segf
 This script does NOT test the output of the function, it only tries to make it segfault.
 
 Made by tmarx
+
 ## Usage
-The script will generate a main.c file that use a static library of libft. So please copy libft.h and libft.a in the same folder as test.py.
+The script will generate a main.c file. It will make your library and link it.
 ```sh
 git clone https://github.com/t0mm4rx/libftdestructor
-cd path-of-your-libft
-make bonus
-cp libft.* path-of-libftdestructor
-cd path-of-libftdestructor
-python3.7 test.py
+cd libftdestructor
+python3.7 path-of-libft [bonus: yes|no]
 ```
+
 ## ToDo
 - [ ] Part 1 functions
 - [x] Bonus functions
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import part2_tests
 import bonus_tests
+import sys
 
 MAIN_BASE1 = f"""#include "libft.h"
 #include <stdio.h>
@@ -26,10 +27,24 @@ int main()
 """
 MAIN_BASE2 = "\n}\n"
 
+BONUS = True
+PATH = "./"
+
+if (len(sys.argv) == 3):
+	PATH = sys.argv[1]
+	if (sys.argv[2] == "yes"):
+		BONUS = True
+	else:
+		BONUS = False
+else:
+	print("Error !\nUsage: python3.7 test.py <path-of-libft> <bonus (yes/no)>\nExemple: python3.7 ../libft yes")
+	exit(0)
+
 def create_tests():
 	tests = ""
 	tests += part2_tests.tests_part2()
-	tests += bonus_tests.tests_bonus()
+	if (BONUS):
+		tests += bonus_tests.tests_bonus()
 	return tests
 
 def create_main():
@@ -39,8 +54,16 @@ def create_main():
 		file.write(tests)
 		file.write(MAIN_BASE2)
 
-def compile_main():
-	os.system("gcc main.c -L. -lft -g -fsanitize=address && ./a.out")
+def compile_lib():
+	os.system("cp {}/libft.h ./".format(PATH))
+	if (BONUS):
+		os.system("make bonus -C {}".format(PATH))
+	else:
+		os.system("make all -C {}".format(PATH))
 
+def compile_main():
+	os.system("gcc main.c -L{} -lft -g -fsanitize=address && ./a.out".format(PATH))
+
+compile_lib()
 create_main()
 compile_main()
